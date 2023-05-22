3098128b98fbdc7d8598652951329500de50b25a
root <bless643>
2023-05-22 16:47:52 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,30 @@
-#!/usr/bin/python3
-"""
-0-gather_data_from_an_API.py
-"""
 import requests
-from sys import argv
+import sys
 
+if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+    print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(argv) < 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        exit(1)
+employee_id = int(sys.argv[1])
 
-    employee_id = argv[1]
+# Make the API request
+response_user = requests.get("https://jsonplaceholder.typicode.com/users/" + str(employee_id))
+response_todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId=" + str(employee_id))
 
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+if response_user.status_code != 200 or response_todos.status_code != 200:
+    print("Failed to retrieve data from the API.")
+    sys.exit(1)
 
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
+user_data = response_user.json()
+todos_data = response_todos.json()
 
-        user_data = user_response.json()
-        todos_data = todos_response.json()
+# Extract relevant information
+employee_name = user_data["name"]
+total_tasks = len(todos_data)
+completed_tasks = sum(1 for todo in todos_data if todo["completed"])
+completed_task_titles = [todo["title"] for todo in todos_data if todo["completed"]]
 
-        employee_name = user_data.get("name")
-
-        total_tasks = len(todos_data)
-        done_tasks = [task for task in todos_data if task.get("completed")]
-
-        done_tasks_count = len(done_tasks)
-
-        print("Employee {} is done with tasks({}/{}):".format(
-            employee_name, done_tasks_count, total_tasks
-        ))
-
-        for task in done_tasks:
-            print("\t{}".format(task.get("title")))
-
-    except requests.exceptions.RequestException as e:
-        print("Error: {}".format(e))
+# Display the information
+print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+for title in completed_task_titles:
+    print(" ", title)
